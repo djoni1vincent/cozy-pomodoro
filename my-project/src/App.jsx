d56a5be25ef9490d49c845972e2d137aaeddc1fe
djoni1vincent <djoni8vincent@gmail.com>
2025-11-13 14:48:19 +0100

    add button that open pop up window  with timer

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -3,13 +3,20 @@ import './App.css'
 import Timer from './components/Timer'
 
 export default function App() {
-  
+ const openPopup = () => {
+    window.open(
+      "/popup.html",
+      "PomodoroTimer",
+      "width=300,height=250,resizable=no"
+    );
+  }
+
   return (
 <>
-
-<div>
-<Timer />
-</div>
+<div className="flex flex-col items-center mt-10">
+      <Timer />
+      
+    </div>
 </>
   )
 }
