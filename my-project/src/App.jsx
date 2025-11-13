dc55abe52fce1deaf7dc9c47201805c333dd9546
djoni1vincent <djoni8vincent@gmail.com>
2025-11-13 13:00:50 +0100

    add func that return 5 min pause after 25 min focus and dynamic bg for pause and focus

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -3,8 +3,10 @@ import './App.css'
 import Timer from './components/Timer'
 
 export default function App() {
+  
   return (
 <>
+
 <div>
 <Timer />
 </div>
--- my-project/src/components/Timer.jsx
+++ my-project/src/components/Timer.jsx
@@ -1,16 +1,19 @@
 import { useEffect, useState } from "react";
 
 export default function Timer() {
-  const [timeLeft, setTimeLeft] = useState(1 * 10);
+  const [timeLeft, setTimeLeft] = useState(25 * 60);
   const [isActive, setIsActive] = useState(false);
   const minutes = Math.floor(timeLeft / 60);
   const seconds = timeLeft % 60;
   const formattedSeconds = seconds.toString().padStart(2, "0");
+    const [mode, setMode] = useState("work" , "break");
+
 
   useEffect(() => {
     if (!isActive) return;
     if (timeLeft === 0) {
-      setIsActive(false);
+      setMode(mode === "work" ? "break" : "work");
+      setTimeLeft(mode === "work" ? 5 * 60 : 25 * 60);
       return;
     }
     const interval = setInterval(() => {
@@ -19,26 +22,31 @@ export default function Timer() {
     return () => clearInterval(interval);
   }, [isActive, timeLeft]);
 
+useEffect(() => {
+  document.body.style.backgroundColor = mode === "work" ? "#987caa" : "#85b2f5";
+}, [mode]);
+
   return (
     <>
       <h2 className="mt-12 font-black font-mono text-6xl ">Pomodoro</h2>
       <div className="[#FFF5E1] mt-80 flex flex-col items-center">
         {" "}
-        <p className="font-mono text-9xl ">
+        <p className="font-mono text-9xl font-black animate-pulse duration-1000 text-[#f7cdfe] ">
           {minutes}:{formattedSeconds}
         </p>
         <button
-          className="bg-red-400 rounded-lg shadow-lg px-8 py-4 mt-10 text-white font-bold text-2xl hover:bg-red-500 transition-colors"
+          className="transition-colors duration-300 bg-red-400 rounded-lg shadow-lg px-8 py-4 mt-4 text-white font-bold text-2xl hover:bg-red-500"
           onClick={() => setIsActive(!isActive)}
         >
           {isActive ? "Pause" : "Start"}
         </button>
-        <button className="mt-2 bg-[#85b2f5] text-[#532e2b] px-4 py-2 rounded-lg shadow hover:bg-[#E6E6FA]" onClick={() => setTimeLeft(1 * 10)}>
+        <button
+          className=" transition-colors duration-300 mt-4 bg-[#85b2f5] text-[#52302d] px-4 py-2 rounded-lg shadow hover:bg-[#a7a7df]"
+          onClick={() => setTimeLeft(1 * 3)}
+        >
           Reset
         </button>
       </div>
-
-
     </>
   );
 }
