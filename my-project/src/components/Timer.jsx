629e67d5ecd3b86f31cbdf599e53d20d0db28a65
djoni1vincent <djoni8vincent@gmail.com>
2025-11-14 13:09:15 +0100

    edit margins

--- my-project/src/components/Timer.jsx
+++ my-project/src/components/Timer.jsx
@@ -35,20 +35,20 @@ export default function Timer() {
 
   return (
     <>
-      <h2 className="mt-12 font-black font-mono text-6xl">Pomodoro</h2>
-      <div id="timer" className="ml-5 mt-80 flex flex-col items-center ">
-        <p className=" text-8xl md:text-9xl font-black animate-pulse duration-1000 text-[#f7cdfe] ">
+      <h2 className="text-purple-200 mt-12 font-black font-mono text-8xl">Pomodoro</h2>
+      <div id="timer" className=" mt-20 md:mt-60 flex flex-col items-center ">
+        <p className=" text-8xl md:mb-10  md:text-9xl font-black animate-pulse duration-1000 text-[#f7cdfe] ">
           {minutes}:{formattedSeconds}
         </p>
 
         <button
-        className="opacity-90 text-xs md:text-xl  transition-colors duration-1000  bg-red-400 rounded-lg shadow-lg px-4 py-2 mt-4 md:px-8 md:py-4 text-white font-bold  hover:bg-red-500"
+        className="opacity-90 text-xs md:text-xl transition-colors duration-1000  bg-red-400 rounded-lg shadow-lg px-4 py-2 mt-4 md:px-8 md:py-4 text-white font-bold  hover:bg-red-500"
           onClick={() => setIsActive(!isActive)}
         >
           {isActive ? "Pause" : "Start"}
         </button>
         <button
-          className="opacity-70 text-xs md:text-xl  transition-colors duration-300 mt-4 bg-[#85b2f5] text-[#52302d] px-2 md:px-4 md:py-2 rounded-lg shadow hover:bg-[#a7a7df]"
+          className="opacity-70 text-xs md:text-xl transition-colors duration-300 mt-2 md:mt-4 bg-[#85b2f5] text-[#52302d] px-2 py-1 md:px-4 md:py-2 rounded-lg shadow hover:bg-[#a7a7df]"
           onClick={() => setTimeLeft(25 * 60)}
         >
           Reset
